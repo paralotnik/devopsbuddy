38be1ba4d95f85a8a46f898cca9b23f77a9f49f2
as_macair <paralotnik@interia.pl>
2017-08-01 23:23:01 +0200

    Added the feedbackto the master

--- src/main/java/com/devopsbuddy/web/controllers/CopyController.java
+++ src/main/java/com/devopsbuddy/web/controllers/CopyController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by tedonema on 03/03/2016.
+ * Created by tedonema on 13/03/2016.
  */
 @Controller
-public class IndexController {
-    @RequestMapping("/")
-    public String home() {
-        return "index";
+public class CopyController {
+
+    @RequestMapping("/about")
+    public String about() {
+        return "copy/about";
     }
 }
